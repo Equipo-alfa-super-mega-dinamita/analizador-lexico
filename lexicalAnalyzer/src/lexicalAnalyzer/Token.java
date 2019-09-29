d739ff3e5569c7f19801cc0f1698977bad6da434
Edder <edhernandezfo@unal.edu.co>
2019-09-29 17:22:03 -0500

    Method toString

--- lexicalAnalyzer/src/lexicalAnalyzer/Token.java
+++ lexicalAnalyzer/src/lexicalAnalyzer/Token.java
@@ -1,9 +1,57 @@
 package lexicalAnalyzer;
 
 public class Token {
-    public int row;
-    public int column;
-    public int idType;
-    public String lexeme;
-    
+    private int row;
+    private int column;
+    private int idType;
+    private String lexeme;
+
+    public Token(int row, int column, String lexeme, int idType) {
+        this.row = row;
+        this.column = column;
+        this.lexeme = lexeme;
+        this.idType = idType;
+    }
+
+    public Token(int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.idType = -1;
+    }
+
+    private String getNameType(int idType) {
+        String ans;
+        switch (idType) {
+            case 1:
+                ans = "id";
+                break;
+            case 2:
+                ans = "integer";
+                break;
+            case 3:
+                ans = "real";
+                break;
+            default:
+                ans = "Fatal Error";
+
+        }
+        return ans;
+    }
+
+    @Override
+    public String toString() {
+        String myTokenString = "";
+        String aux = getNameType(this.idType);
+        if (aux.equals(this.lexeme)) {
+            myTokenString = "<" + aux + "," + row + "," + column + ">";
+        } else {
+            myTokenString = "<" + aux + "," + lexeme + "," + row + "," + column + ">";
+        }
+        return myTokenString;
+    }
+
+    public static final int
+            ID = 1,
+            INT = 2,
+            REAL = 3;
 }
--- lexicalAnalyzer/src/port/LexicalSyntaxBridge.java
+++ lexicalAnalyzer/src/port/LexicalSyntaxBridge.java
@@ -1,4 +1,5 @@
 package port;
 
 public class LexicalSyntaxBridge {
+
 }
