3c9760cbe497320f2af0afaf5de0a8bc9c7456db
Nicolai Romero <anromerom@unal.edu.co>
2019-09-30 18:18:29 -0500

    Auto stash before merge of "master" and "origin/master"

--- lexicalAnalyzer/src/symboltable/SymbolTable.java
+++ lexicalAnalyzer/src/symboltable/SymbolTable.java
@@ -5,94 +5,99 @@ import java.util.HashSet;
 public class SymbolTable {
     HashSet<String> reservedWordList;
     HashSet<String> enAlphabet;
+    private static final String[] reservedWords = {
+            "af",
+            "and",
+            "any",
+            "begin",
+            "body",
+            "bool",
+            "by",
+            "call",
+            "cap",
+            "char",
+            "co",
+            "const",
+            "create",
+            "destroy",
+            "do",
+            "downto",
+            "else",
+            "end",
+            "enum",
+            "exit",
+            "extend",
+            "external",
+            "fa",
+            "false",
+            "fi",
+            "return",
+            "file",
+            "final",
+            "forward",
+            "global",
+            "high",
+            "if",
+            "import",
+            "in",
+            "initial",
+            "int",
+            "low",
+            "mod",
+            "new",
+            "next",
+            "ni",
+            "noop",
+            "not",
+            "null",
+            "oc",
+            "od",
+            "on",
+            "op",
+            "optype",
+            "or",
+            "p",
+            "proc",
+            "procedure",
+            "process",
+            "ptr",
+            "real",
+            "rec",
+            "receive",
+            "ref",
+            "reply",
+            "res",
+            "resource",
+            "return",
+            "returns",
+            "sem",
+            "send",
+            "separate",
+            "skip",
+            "st",
+            "stderr",
+            "stdin",
+            "stdout",
+            "stop",
+            "string",
+            "to",
+            "true",
+            "type",
+            "union",
+            "v",
+            "val",
+            "var",
+            "vm",
+            "xor"
+    };
     public SymbolTable(){
         reservedWordList = new HashSet<>();
         IniResWordList();
     }
     private void IniResWordList(){
-        reservedWordList.add("af");
-        reservedWordList.add("and");
-        reservedWordList.add("any");
-        reservedWordList.add("begin");
-        reservedWordList.add("body");
-        reservedWordList.add("bool");
-        reservedWordList.add("by");
-        reservedWordList.add("call");
-        reservedWordList.add("cap");
-        reservedWordList.add("char");
-        reservedWordList.add("co");
-        reservedWordList.add("const");
-        reservedWordList.add("create");
-        reservedWordList.add("destroy");
-        reservedWordList.add("do");
-        reservedWordList.add("downto");
-        reservedWordList.add("else");
-        reservedWordList.add("end");
-        reservedWordList.add("enum");
-        reservedWordList.add("exit");
-        reservedWordList.add("extend");
-        reservedWordList.add("external");
-        reservedWordList.add("fa");
-        reservedWordList.add("false");
-        reservedWordList.add("fi");
-        reservedWordList.add("return");
-        reservedWordList.add("file");
-        reservedWordList.add("final");
-        reservedWordList.add("forward");
-        reservedWordList.add("global");
-        reservedWordList.add("high");
-        reservedWordList.add("if");
-        reservedWordList.add("import");
-        reservedWordList.add("in");
-        reservedWordList.add("initial");
-        reservedWordList.add("int");
-        reservedWordList.add("low");
-        reservedWordList.add("mod");
-        reservedWordList.add("new");
-        reservedWordList.add("next");
-        reservedWordList.add("ni");
-        reservedWordList.add("noop");
-        reservedWordList.add("not");
-        reservedWordList.add("null");
-        reservedWordList.add("oc");
-        reservedWordList.add("od");
-        reservedWordList.add("on");
-        reservedWordList.add("op");
-        reservedWordList.add("optype");
-        reservedWordList.add("or");
-        reservedWordList.add("p");
-        reservedWordList.add("proc");
-        reservedWordList.add("procedure");
-        reservedWordList.add("process");
-        reservedWordList.add("ptr");
-        reservedWordList.add("real");
-        reservedWordList.add("rec");
-        reservedWordList.add("receive");
-        reservedWordList.add("ref");
-        reservedWordList.add("reply");
-        reservedWordList.add("res");
-        reservedWordList.add("resource");
-        reservedWordList.add("return");
-        reservedWordList.add("returns");
-        reservedWordList.add("sem");
-        reservedWordList.add("send");
-        reservedWordList.add("separate");
-        reservedWordList.add("skip");
-        reservedWordList.add("st");
-        reservedWordList.add("stderr");
-        reservedWordList.add("stdin");
-        reservedWordList.add("stdout");
-        reservedWordList.add("stop");
-        reservedWordList.add("string");
-        reservedWordList.add("to");
-        reservedWordList.add("true");
-        reservedWordList.add("type");
-        reservedWordList.add("union");
-        reservedWordList.add("v");
-        reservedWordList.add("val");
-        reservedWordList.add("var");
-        reservedWordList.add("vm");
-        reservedWordList.add("xor");
+        for( String s : reservedWords){
+            reservedWordList.add("xor");
+        }
     }
 
     public boolean isResWord(String word){
@@ -101,7 +106,7 @@ public class SymbolTable {
     }
 
     public SymbolTable(boolean bool){
-        enAlphabet = new HashSet<>();
+        enAlphabet = new HashSet<String>();
         iniEnAlphabet();
     }
 
